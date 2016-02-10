a7d83bb28dd6baf605131d65851e4ebd07b1d0d6
Vito <Underskin1@hotmail.com>
2016-02-10 16:15:43 +0100

    langAppvs2
    Frond-end

---
+++ assets/js/routes.js
@@ -0,0 +1,10 @@
+var foundationRoutes = [{
+    "name": "home",
+    "url": "/",
+    "path": "templates/home.html"
+}, {
+    "name": "abc",
+    "url": "/",
+    "path": "templates/abc.html"
+}];
+
